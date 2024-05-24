280fceb874c871791d966276ca7b9e9fbbb7cb33
sidey383 <sidey383@mail.ru>
2024-05-25 04:27:01 +0700

    Remove unused import

--- src/main/java/ru/sidey383/IntervalsRestAPI.java
+++ src/main/java/ru/sidey383/IntervalsRestAPI.java
@@ -2,7 +2,6 @@ package ru.sidey383;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
